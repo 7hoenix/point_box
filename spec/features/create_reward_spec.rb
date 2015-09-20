12ec9460b02e1b37a5432d0ec5f604ca656774f6
philmphoenix <jpholzmann@gmail.com>
2015-09-20 15:54:13 -0600

    WIP create rewards

---
+++ spec/features/create_reward_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.feature "An admin creates a new reward" do
+  scenario "and is redirected to the listing of all rewards" do
+    User.create(username: "justin", password: "justin", role: 1)
+
+    visit login_path
+    fill_in "Username", with: "justin"
+    fill_in "Password", with: "justin"
+    click_button "Login"
+
+    click_link "Create new reward"
+
+    fill_in "Name", with: "brownies"
+    select 10, from: "Cost"
+    click_button "Create reward"
+
+    expect(page).to have_content("brownies: 10")
+  end
+end
