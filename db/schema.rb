05edf823f37f26b344ce6a9c480dd4570401d3f8
philmphoenix <jpholzmann@gmail.com>
2015-09-20 15:33:46 -0600

    Created points columns and admin functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150917221230) do
+ActiveRecord::Schema.define(version: 20150920210815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,11 @@ ActiveRecord::Schema.define(version: 20150917221230) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.integer  "role",            default: 0
+    t.integer  "points",          default: 0
+    t.integer  "redeemed_points", default: 0
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#is_admin?" do
+    it "returns true if the user is an admin" do
+      admin = User.create(username: "justin", password: "justin", role: 1)
+
+      expect(admin.is_admin?).to be true
+    end
+
+    it "returns false if the user is a hacker like stinette" do
+      hacker_stinette = User.create(username: "stinette", password: "stinette")
+
+      expect(hacker_stinette.is_admin?).to be false
+    end
+  end
+
+  describe "has attributes" do
+    it "has a no points and a role of default by default" do
+      stinette = User.create(username: "stinette", password: "stinette")
+
+      expect(stinette.role).to eq("default")
+      expect(stinette.points).to eq(0)
+    end
+  end
 end
